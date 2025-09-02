fc7d56c19a2de3762a6a837a930a64bd067e354e
Bablu Agrahari <babluagrahari@Bablus-MacBook-Air.local>
2025-09-02 11:48:07 +0530

    new commit

---
+++ src/main/java/com/example/springairedis/SpringAiRedisApplication.java
@@ -0,0 +1,11 @@
+package com.example.springairedis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiRedisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiRedisApplication.class, args);
+    }
+}
